cbed5fce91cd774914b218b729769a90c3fdc865
Ekaterina Kunets <e.kunets@fantasyinvest.club>
2022-05-25 17:35:08 +0300

    hot && cold streams

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,45 @@
-import { Observable } from "rxjs";
+// import { Observable } from "rxjs";
+//
+// const sequence$ = new Observable((subscriber) => {
+//   let count = 1;
+//   const intervalId = setInterval(() => {
+//     subscriber.next(count++);
+//     if (count % 5 === 0) {
+//       clearInterval(intervalId);
+//       subscriber.complete();
+//       return
+//     }
+//   }, 1000);
+//   return () => {
+//     console.log('unsubscribe');
+//     clearInterval(intervalId);
+//   }
+// });
+//
+// const subscription = sequence$.subscribe(
+//   (v) => console.log(v) ,
+//   (err) => console.log(err) ,
+//   () => console.log('complete')
+// );
+//
+// setInterval(() => {
+//   subscription.unsubscribe()
+// }, 3000);
 
-const sequence$ = new Observable((subscriber) => {
-  let count = 1;
-  const intervalId = setInterval(() => {
-    subscriber.next(count++);
-    if (count % 5 === 0) {
-      clearInterval(intervalId);
-      subscriber.complete();
-      return
-    }
-  }, 1000);
-  return () => {
-    console.log('unsubscribe');
-    clearInterval(intervalId);
-  }
+import { fromEvent, interval } from 'rxjs';
+
+const sequence$ = fromEvent<MouseEvent>(document, 'click')
+
+const sub1 = sequence$.subscribe((v) => {
+  console.log('Sub 1', v.clientX);
 });
 
-const subscription = sequence$.subscribe(
-  (v) => console.log(v) ,
-  (err) => console.log(err) ,
-  () => console.log('complete')
-);
+// unsubscribe - не завершает поток, только отписка. Завршает complete
+// Холодный поток - получаешь все значения с самого начала
+// Горячий поток - получаешь значения с момента подписки
+setTimeout(() => {
+  sequence$.subscribe((v) => {
+    console.log('Sub 2', v.clientX);
+  });
+}, 5000);
 
-setInterval(() => {
-  subscription.unsubscribe()
-}, 3000);
