fe4517ee699598d6feffe457f04dc9dda27a757b
Ekaterina Kunets <e.kunets@fantasyinvest.club>
2022-05-25 17:02:44 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Observable } from "rxjs";
+
+const sequence$ = new Observable((subscriber) => {
+  let count = 1;
+  const intervalId = setInterval(() => {
+    subscriber.next(count++);
+    if (count % 5 === 0) {
+      clearInterval(intervalId);
+      subscriber.complete();
+      return
+    }
+  }, 1000);
+  return () => {
+    console.log('unsubscribe');
+    clearInterval(intervalId);
+  }
+});
+
+const subscription = sequence$.subscribe(
+  (v) => console.log(v) ,
+  (err) => console.log(err) ,
+  () => console.log('complete')
+);
+
+setInterval(() => {
+  subscription.unsubscribe()
+}, 3000);
