aca54c9523eb9b927b3bba79d5496dd2abc7324a
Ekaterina Kunets <e.kunets@fantasyinvest.club>
2022-05-27 14:43:45 +0300

    hot && cold streams prt 2

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
-// import { Observable } from "rxjs";
-//
-// const sequence$ = new Observable((subscriber) => {
-//   let count = 1;
-//   const intervalId = setInterval(() => {
-//     subscriber.next(count++);
-//     if (count % 5 === 0) {
-//       clearInterval(intervalId);
-//       subscriber.complete();
-//       return
-//     }
-//   }, 1000);
-//   return () => {
-//     console.log('unsubscribe');
-//     clearInterval(intervalId);
-//   }
-// });
+import { Observable } from "rxjs";
+
+const socket: WebSocket = new WebSocket('wss://echo.websocket.org')
+const sequence$ = new Observable((subscriber) => {
+  socket.addEventListener('message', (e) => subscriber.next(e));
+
+  // return () => {
+  //   console.log('unsubscribe');
+  //   clearInterval(intervalId);
+  // }
+});
 //
 // const subscription = sequence$.subscribe(
 //   (v) => console.log(v) ,
@@ -26,20 +20,26 @@
 //   subscription.unsubscribe()
 // }, 3000);
 
-import { fromEvent, interval } from 'rxjs';
-
-const sequence$ = fromEvent<MouseEvent>(document, 'click')
-
-const sub1 = sequence$.subscribe((v) => {
-  console.log('Sub 1', v.clientX);
+// import { fromEvent, interval } from 'rxjs';
+//
+// const sequence$ = fromEvent<MouseEvent>(document, 'click')
+//
+const sub1 = sequence$.subscribe((v: any) => {
+  console.log('Sub 1', v.data);
 });
+
+socket.addEventListener('open', () => {
+  let count = 1;
+  const intervalId = setInterval(() => {
+    socket.send((count++).toString())
+  }, 1000);
+})
 
 // unsubscribe - не завершает поток, только отписка. Завршает complete
 // Холодный поток - получаешь все значения с самого начала
 // Горячий поток - получаешь значения с момента подписки
 setTimeout(() => {
-  sequence$.subscribe((v) => {
-    console.log('Sub 2', v.clientX);
+  sequence$.subscribe((v: any) => {
+    console.log('Sub 2', v.data);
   });
 }, 5000);
-
